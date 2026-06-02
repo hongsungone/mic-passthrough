10d53b8259f3a9a6b0df728609eb9822074d48eb
Lium Hong <hongsungone@Liums-MacBook-Pro.local>
2026-06-02 18:14:52 -0400

    Default broadcast to en0 WiFi

--- mic_passthrough/mac_app.py
+++ mic_passthrough/mac_app.py
@@ -37,7 +37,11 @@ class MicPassthroughApp(rumps.App):
         self.discovered = {}
         self.local_ips = get_local_ips()
         print("Local IPs found:", self.local_ips)
-        self.selected_local_ip = self.local_ips[0][1] if self.local_ips else None
+        # prefer en0 (WiFi) as default
+        self.selected_local_ip = next(
+            (ip for iface, ip in self.local_ips if iface == 'en0'),
+            self.local_ips[0][1] if self.local_ips else None
+        )
 
         ip_items = []
         for iface, ip in self.local_ips:
